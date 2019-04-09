880a4bb191d4deced2fd746aaf0016a47759ea97
huangshuai <chimmhuang@163.com>
2019-04-09 17:37:29 +0800

    fix: 修复了mac路径的问题

--- 03-freemarker/src/test/java/com/freemarker/test/freemarker/FreemarkerTest.java
+++ 03-freemarker/src/test/java/com/freemarker/test/freemarker/FreemarkerTest.java
@@ -1,5 +1,6 @@
-package com.xuecheng.test.freemarker;
+package com.freemarker.test.freemarker;
 
+import com.freemarker.FreemarkerTestApplication;
 import com.freemarker.model.Student;
 import freemarker.cache.StringTemplateLoader;
 import freemarker.template.Configuration;
@@ -9,10 +10,12 @@ import org.apache.commons.io.IOUtils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.ui.freemarker.FreeMarkerTemplateUtils;
 
 import java.io.*;
+import java.net.URLDecoder;
 import java.util.*;
 
 /**
@@ -20,8 +23,6 @@ import java.util.*;
  * @version 1.0
  * @create 2018-06-13 10:07
  **/
-@SpringBootTest
-@RunWith(SpringRunner.class)
 public class FreemarkerTest {
 
     //基于模板生成静态化文件
@@ -29,7 +30,8 @@ public class FreemarkerTest {
     public void testGenerateHtml() throws IOException, TemplateException {
         //创建配置类
         Configuration configuration=new Configuration(Configuration.getVersion());
-        String classpath = this.getClass().getResource("/").getPath();
+        String classpath = URLDecoder.decode(this.getClass().getResource("/").getPath(),"utf-8");
+
         //设置模板路径
         configuration.setDirectoryForTemplateLoading(new File(classpath + "/templates/"));
         //设置字符集
@@ -44,8 +46,43 @@ public class FreemarkerTest {
         System.out.println(content);
         InputStream inputStream = IOUtils.toInputStream(content);
         //输出文件
-        FileOutputStream fileOutputStream = new FileOutputStream(new File("d:/test1.html"));
+        FileOutputStream fileOutputStream = new FileOutputStream(new File("/Users/huangshuai/IdeaProjects/实验室/03-freemarker/src/test/resources/test1.html"));
         int copy = IOUtils.copy(inputStream, fileOutputStream);
+    }
+
+    //数据模型
+    private Map getMap(){
+        Map<String, Object> map = new HashMap<>();
+        //向数据模型放数据
+        map.put("name","吃茫茫");
+        Student stu1 = new Student();
+        stu1.setName("小明");
+        stu1.setAge(18);
+        stu1.setMondy(1000.86f);
+        stu1.setBirthday(new Date());
+        Student stu2 = new Student();
+        stu2.setName("小红");
+        stu2.setMondy(200.1f);
+        stu2.setAge(19);
+//        stu2.setBirthday(new Date());
+        List<Student> friends = new ArrayList<>();
+        friends.add(stu1);
+        stu2.setFriends(friends);
+        stu2.setBestFriend(stu1);
+        List<Student> stus = new ArrayList<>();
+        stus.add(stu1);
+        stus.add(stu2);
+        //向数据模型放数据
+        map.put("stus",stus);
+        //准备map数据
+        HashMap<String,Student> stuMap = new HashMap<>();
+        stuMap.put("stu1",stu1);
+        stuMap.put("stu2",stu2);
+        //向数据模型放数据
+        map.put("stu1",stu1);
+        //向数据模型放数据
+        map.put("stuMap",stuMap);
+        return map;
     }
 
     //基于模板字符串生成静态化文件
@@ -80,40 +117,5 @@ public class FreemarkerTest {
         //输出文件
         FileOutputStream fileOutputStream = new FileOutputStream(new File("d:/test1.html"));
         IOUtils.copy(inputStream, fileOutputStream);
-    }
-
-    //数据模型
-    private Map getMap(){
-        Map<String, Object> map = new HashMap<>();
-        //向数据模型放数据
-        map.put("name","黑马程序员");
-        Student stu1 = new Student();
-        stu1.setName("小明");
-        stu1.setAge(18);
-        stu1.setMondy(1000.86f);
-        stu1.setBirthday(new Date());
-        Student stu2 = new Student();
-        stu2.setName("小红");
-        stu2.setMondy(200.1f);
-        stu2.setAge(19);
-//        stu2.setBirthday(new Date());
-        List<Student> friends = new ArrayList<>();
-        friends.add(stu1);
-        stu2.setFriends(friends);
-        stu2.setBestFriend(stu1);
-        List<Student> stus = new ArrayList<>();
-        stus.add(stu1);
-        stus.add(stu2);
-        //向数据模型放数据
-        map.put("stus",stus);
-        //准备map数据
-        HashMap<String,Student> stuMap = new HashMap<>();
-        stuMap.put("stu1",stu1);
-        stuMap.put("stu2",stu2);
-        //向数据模型放数据
-        map.put("stu1",stu1);
-        //向数据模型放数据
-        map.put("stuMap",stuMap);
-        return map;
     }
 }
--- 03-freemarker/src/test/java/com/freemarker/test/freemarker/testRestTemplate.java
+++ 03-freemarker/src/test/java/com/freemarker/test/freemarker/testRestTemplate.java
@@ -1,4 +1,4 @@
-package com.xuecheng.test.freemarker;
+package com.freemarker.test.freemarker;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
