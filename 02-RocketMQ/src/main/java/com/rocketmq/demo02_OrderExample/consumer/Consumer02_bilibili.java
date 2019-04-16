ae952d57a96e740d3f37a95fb7ec97a6898654f0
huangshuai <chimmhuang@163.com>
2019-04-16 14:39:57 +0800

    feat: 更新了顺序消费(Order)信息的demo代码

---
+++ 02-RocketMQ/src/main/java/com/rocketmq/demo02_OrderExample/consumer/Consumer02_bilibili.java
@@ -0,0 +1,73 @@
+package com.rocketmq.demo02_OrderExample.consumer;
+
+import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
+import org.apache.rocketmq.client.consumer.listener.*;
+import org.apache.rocketmq.common.message.MessageExt;
+import org.apache.rocketmq.remoting.common.RemotingHelper;
+
+import java.io.UnsupportedEncodingException;
+import java.util.List;
+
+/**
+ * @author huangshuai
+ * @date 2019-04-06 19:32
+ *
+ * 顺序消费者
+ */
+public class Consumer02_bilibili {
+
+    public static void main(String[] args) throws Exception{
+        //实例化一个消费者（可以自定义名称，默认：DEFAULT_CONSUMER）
+        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("demo02");
+
+        //指定消息地址
+        consumer.setNamesrvAddr("172.16.120.132:9876");
+
+        /**
+         * 订阅要消费的Topic
+         * @param topic
+         * @param subExpression 订阅表达式，如："tag1 || tag2"，如果为null或者为*，则表示订阅全部
+         */
+
+        consumer.subscribe("TopicOrderTest","*");
+
+        consumer.setVipChannelEnabled(false);
+
+        //创建消息监听MessageListenner
+        consumer.setMessageListener(new MessageListenerOrderly() {
+            @Override
+            public ConsumeOrderlyStatus consumeMessage(List<MessageExt> msgs, ConsumeOrderlyContext context) {
+                for (MessageExt msg : msgs) {
+                    try {
+
+                        //获取主题
+                        String topic = msg.getTopic();
+
+                        //获取标签
+                        String tags = msg.getTags();
+
+                        //获取信息
+                        byte[] body = msg.getBody();
+                        String result = new String(body, RemotingHelper.DEFAULT_CHARSET);
+
+                        System.out.println("Order--Consumer消费信息---topic:" + topic + ",tags:" + tags + ",result:" + result);
+
+                    } catch (UnsupportedEncodingException e) {
+                        e.printStackTrace();
+
+                        //消息重试
+                        return ConsumeOrderlyStatus.SUSPEND_CURRENT_QUEUE_A_MOMENT;
+                    }
+                }
+
+                //成功消费消息
+                return ConsumeOrderlyStatus.SUCCESS;
+            }
+        });
+
+        //开启消费者实例
+        consumer.start();
+
+        System.out.println("Consumer Started");
+    }
+}
--- 02-RocketMQ/src/main/java/com/rocketmq/demo02_OrderExample/producer/OrderedProducer.java
+++ 02-RocketMQ/src/main/java/com/rocketmq/demo02_OrderExample/producer/OrderedProducer.java
@@ -13,7 +13,7 @@ import java.util.List;
  * @author huangshuai
  * @date 2019-04-08 21:57
  *
- * 订单发送消息示例代码
+ * 顺序发送消息示例代码
  */
 public class OrderedProducer {
 
@@ -28,10 +28,11 @@ public class OrderedProducer {
         //加载生产者
         producer.start();
 
-        String[] tags = new String[]{"TagA", "TagB", "TagC", "TagD", "TagE"};
+        //String[] tags = new String[]{"TagA", "TagB", "TagC", "TagD", "TagE"};
+        String[] tags = new String[]{"TagA"};
         for (int i = 0; i < 10; i++) {
 
-            int orderId = i % 10;
+            //int orderId = i % 10;
 
             /**
              * 创建消息实例，指定topic，tag，和正文内容
@@ -39,17 +40,36 @@ public class OrderedProducer {
              * @param tags
              * @param body 消息内容
              */
-            Message message = new Message("TopicTest",
+            Message message = new Message("TopicOrderTest",
                     tags[i % tags.length],
                     "KEY"+i,
                     ("你好RocketMQ"+i).getBytes(RemotingHelper.DEFAULT_CHARSET));
+
+            /**
+             * 顺序发送消息
+             * 第一个参数：发送的消息信息
+             * 第二个参数：选中指定的消息队列对象（会将所有消息队列传入进来）
+             * 第三个参数：指定对应的队列下标
+             */
             SendResult sendResult = producer.send(message, new MessageQueueSelector() {
+                /**
+                 * 队列选择
+                 * @param mqs 队列集合（如果是集群，那么就是集群总共的队列集合）
+                 * @param msg 发送的消息
+                 * @param arg 指定队列的下标
+                 * @return
+                 */
+                @Override
                 public MessageQueue select(List<MessageQueue> mqs, Message msg, Object arg) {
-                    Integer id = (Integer) arg;
-                    int index = id % mqs.size();
+                    //获取队列的下标
+                    Integer index = (Integer) arg;
+
+                    //int index = id % mqs.size();
+
+                    //获取对应下标的队列
                     return mqs.get(index);
                 }
-            },orderId);
+            },0);
 
             System.out.printf("%s%n",sendResult);
         }
