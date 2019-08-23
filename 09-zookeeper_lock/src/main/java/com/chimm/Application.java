53742f0d8034dcbe79708b0aeeb5c8d34c5a24f6
huangshuai <chimmhuang@163.com>
2019-08-23 09:36:54 +0800

    test: 测试数据库插入

--- 09-zookeeper_lock/src/main/java/com/chimm/Application.java
+++ 09-zookeeper_lock/src/main/java/com/chimm/Application.java
@@ -2,12 +2,14 @@ package com.chimm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tk.mybatis.spring.annotation.MapperScan;
 
 /**
  * @author chimm
  * @date 2019/8/18 0018
  */
 @SpringBootApplication
+@MapperScan(basePackages = {"com.chimm.mapper"})
 public class Application {
 
     public static void main(String[] args) {
