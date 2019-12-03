a525885fcda33df842b5ad20c9e6197b81001d7c
chimmhuang <chimmhuang@163.com>
2019-12-03 18:03:02 +0800

    feat: 新增添加好友的功能

--- 12-muxin/src/main/java/com/chimm/muxin/controller/UserController.java
+++ 12-muxin/src/main/java/com/chimm/muxin/controller/UserController.java
@@ -2,7 +2,10 @@ package com.chimm.muxin.controller;
 
 import com.chimm.muxin.domain.Users;
 import com.chimm.muxin.domain.bo.UsersBO;
+import com.chimm.muxin.domain.vo.MyFriendsVO;
 import com.chimm.muxin.domain.vo.UsersVO;
+import com.chimm.muxin.enums.OperatorFriendRequestTypeEnum;
+import com.chimm.muxin.enums.SearchFriendsStatusEnum;
 import com.chimm.muxin.service.UserService;
 import com.chimm.muxin.utils.FastDFSClient;
 import com.chimm.muxin.utils.FileUtils;
@@ -11,11 +14,14 @@ import com.chimm.muxin.utils.MD5Utils;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 /**
  * 用户模块
@@ -24,7 +30,7 @@ import org.springframework.web.multipart.MultipartFile;
  * @date 2019/11/5
  */
 @RestController
-@RequestMapping("/user")
+@RequestMapping(value = "/u",produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
 public class UserController {
 
     @Autowired
@@ -111,5 +117,102 @@ public class UserController {
         Users result = userService.updateUserInfo(user);
 
         return IMoocJSONResult.ok(result);
+    }
+
+    /**
+     * @Description: 搜索好友接口, 根据账号做匹配查询而不是模糊查询
+     */
+    @PostMapping("/search")
+    public IMoocJSONResult searchUser(String myUserId, String friendUsername) throws Exception {
+
+        // 0. 判断 myUserId friendUsername 不能为空
+        if (StringUtils.isBlank(myUserId) || StringUtils.isBlank(friendUsername)) {
+            return IMoocJSONResult.errorMsg("");
+        }
+
+        // 前置条件 - 1. 搜索的用户如果不存在，返回[无此用户]
+        // 前置条件 - 2. 搜索账号是你自己，返回[不能添加自己]
+        // 前置条件 - 3. 搜索的朋友已经是你的好友，返回[该用户已经是你的好友]
+        Integer status = userService.preconditionSearchFriends(myUserId, friendUsername);
+        if (status.equals(SearchFriendsStatusEnum.SUCCESS.status)) {
+            Users user = userService.queryUserInfoByUsername(friendUsername);
+            UsersVO userVO = new UsersVO();
+            BeanUtils.copyProperties(user, userVO);
+            return IMoocJSONResult.ok(userVO);
+        } else {
+            String errorMsg = SearchFriendsStatusEnum.getMsgByKey(status);
+            return IMoocJSONResult.errorMsg(errorMsg);
+        }
+    }
+
+    /**
+     * @Description: 发送添加好友的请求
+     */
+    @PostMapping(value = "/addFriendRequest", produces = "application/json")
+    public IMoocJSONResult addFriendRequest(String myUserId, String friendUsername) throws Exception {
+
+        // 0. 判断 myUserId friendUsername 不能为空
+        if (StringUtils.isBlank(myUserId) || StringUtils.isBlank(friendUsername)) {
+            return IMoocJSONResult.errorMsg("");
+        }
+
+        // 前置条件 - 1. 搜索的用户如果不存在，返回[无此用户]
+        // 前置条件 - 2. 搜索账号是你自己，返回[不能添加自己]
+        // 前置条件 - 3. 搜索的朋友已经是你的好友，返回[该用户已经是你的好友]
+        Integer status = userService.preconditionSearchFriends(myUserId, friendUsername);
+        if (status.equals(SearchFriendsStatusEnum.SUCCESS.status)) {
+            userService.sendFriendRequest(myUserId, friendUsername);
+        } else {
+            String errorMsg = SearchFriendsStatusEnum.getMsgByKey(status);
+            return IMoocJSONResult.errorMsg(errorMsg);
+        }
+
+        return IMoocJSONResult.ok();
+    }
+
+    /**
+     * @Description: 查询添加好友的请求
+     */
+    @PostMapping("/queryFriendRequests")
+    public IMoocJSONResult queryFriendRequests(String userId) {
+
+        // 0. 判断不能为空
+        if (StringUtils.isBlank(userId)) {
+            return IMoocJSONResult.errorMsg("");
+        }
+
+        // 1. 查询用户接受到的朋友申请
+        return IMoocJSONResult.ok(userService.queryFriendRequestList(userId));
+    }
+
+    /**
+     * @Description: 接受方 通过或者忽略朋友请求
+     */
+    @PostMapping("/operFriendRequest")
+    public IMoocJSONResult operFriendRequest(String acceptUserId, String sendUserId, Integer operType) {
+
+        // 0. acceptUserId sendUserId openType 判断不能为空
+        if (StringUtils.isBlank(acceptUserId) || StringUtils.isBlank(sendUserId) || operType == null) {
+            return IMoocJSONResult.errorMsg("");
+        }
+
+        // 1. 如果openType 没有对应的枚举值，则直接抛出空错误信息
+        if (StringUtils.isBlank(OperatorFriendRequestTypeEnum.getMsgByType(operType))) {
+            return IMoocJSONResult.errorMsg("");
+        }
+
+        if (operType.equals(OperatorFriendRequestTypeEnum.IGNORE.type)) {
+            // 2. 判断如果忽略好友请求，则直接删除好友请求的数据库表记录
+            userService.deleteFriendRequest(sendUserId, acceptUserId);
+        } else if (operType.equals(OperatorFriendRequestTypeEnum.PASS.type)) {
+            // 3. 判断如果是通过好友请求，则互相增加好友记录到数据库对应的表
+            //	   然后删除好友请求的数据库表记录
+            userService.passFriendRequest(sendUserId, acceptUserId);
+        }
+
+        // 4. 数据库查询好友列表
+//        List<MyFriendsVO> myFriends = userService.queryMyFriends(acceptUserId);
+
+        return IMoocJSONResult.ok(/*myFriends*/);
     }
 }
--- 12-muxin/src/main/java/com/chimm/muxin/service/impl/UserServiceImpl.java
+++ 12-muxin/src/main/java/com/chimm/muxin/service/impl/UserServiceImpl.java
@@ -1,6 +1,12 @@
 package com.chimm.muxin.service.impl;
 
+import com.chimm.muxin.domain.FriendsRequest;
+import com.chimm.muxin.domain.MyFriends;
 import com.chimm.muxin.domain.Users;
+import com.chimm.muxin.domain.vo.FriendRequestVO;
+import com.chimm.muxin.enums.SearchFriendsStatusEnum;
+import com.chimm.muxin.mapper.FriendsRequestMapper;
+import com.chimm.muxin.mapper.MyFriendsMapper;
 import com.chimm.muxin.mapper.UsersMapper;
 import com.chimm.muxin.service.UserService;
 import com.chimm.muxin.utils.FastDFSClient;
@@ -16,6 +22,8 @@ import org.springframework.web.multipart.MultipartFile;
 import tk.mybatis.mapper.entity.Example;
 
 import java.io.IOException;
+import java.util.Date;
+import java.util.List;
 
 /**
  * @author huangshuai
@@ -33,6 +41,10 @@ public class UserServiceImpl implements UserService {
     private QRCodeUtils qrCodeUtils;
     @Autowired
     private FastDFSClient fastDFSClient;
+    @Autowired
+    private MyFriendsMapper myFriendsMapper;
+    @Autowired
+    private FriendsRequestMapper friendsRequestMapper;
 
     @Transactional(propagation = Propagation.SUPPORTS)
     @Override
@@ -94,5 +106,118 @@ public class UserServiceImpl implements UserService {
     @Transactional(propagation = Propagation.SUPPORTS)
     protected Users queryUserById(String userId) {
         return userMapper.selectByPrimaryKey(userId);
+    }
+
+    @Transactional(propagation = Propagation.SUPPORTS, rollbackFor = Exception.class)
+    @Override
+    public Integer preconditionSearchFriends(String myUserId, String friendUsername) {
+
+        Users user = queryUserInfoByUsername(friendUsername);
+
+        // 1. 搜索的用户如果不存在，返回[无此用户]
+        if (user == null) {
+            return SearchFriendsStatusEnum.USER_NOT_EXIST.status;
+        }
+
+        // 2. 搜索账号是你自己，返回[不能添加自己]
+        if (user.getId().equals(myUserId)) {
+            return SearchFriendsStatusEnum.NOT_YOURSELF.status;
+        }
+
+        // 3. 搜索的朋友已经是你的好友，返回[该用户已经是你的好友]
+        Example example = Example.builder(MyFriends.class)
+                .build();
+        example.createCriteria()
+                .andEqualTo("myUserId", myUserId)
+                .andEqualTo("myFriendUserId",user.getId());
+
+        MyFriends myFriends = myFriendsMapper.selectOneByExample(example);
+
+        if (myFriends != null) {
+            return SearchFriendsStatusEnum.ALREADY_FRIENDS.status;
+        }
+
+        return SearchFriendsStatusEnum.SUCCESS.status;
+    }
+
+    @Transactional(propagation = Propagation.SUPPORTS)
+    @Override
+    public Users queryUserInfoByUsername(String username) {
+        return userMapper.selectOne(Users.builder()
+                .username(username)
+                .build());
+    }
+
+    @Transactional(propagation = Propagation.REQUIRED, rollbackFor = Exception.class)
+    @Override
+    public void sendFriendRequest(String myUserId, String friendUsername) {
+
+        // 根据用户名把朋友信息查询出来
+        Users friend = queryUserInfoByUsername(friendUsername);
+
+        // 1. 查询发送好友请求记录表
+        Example example = Example.builder(FriendsRequest.class)
+                .build();
+        example.createCriteria()
+                .andEqualTo("sendUserId", myUserId)
+                .andEqualTo("acceptUserId", friend.getId());
+        FriendsRequest friendsRequest = friendsRequestMapper.selectOneByExample(example);
+
+        if (friendsRequest == null) {
+            // 2. 如果不是你的好友，并且好友记录没有添加，则新增好友请求记录
+            String requestId = sid.nextShort();
+
+            friendsRequestMapper.insert(FriendsRequest.builder()
+                    .id(requestId)
+                    .sendUserId(myUserId)
+                    .acceptUserId(friend.getId())
+                    .requestDateTime(new Date())
+                    .build());
+        }
+    }
+
+    @Transactional(propagation = Propagation.SUPPORTS, rollbackFor = Exception.class)
+    @Override
+    public List<FriendRequestVO> queryFriendRequestList(String acceptUserId) {
+        return userMapper.queryFriendRequestList(acceptUserId);
+    }
+
+    @Transactional(propagation = Propagation.REQUIRED, rollbackFor = Exception.class)
+    @Override
+    public void deleteFriendRequest(String sendUserId, String acceptUserId) {
+        friendsRequestMapper.delete(FriendsRequest.builder()
+                .sendUserId(sendUserId)
+                .acceptUserId(acceptUserId)
+                .build());
+    }
+
+    @Transactional(propagation = Propagation.REQUIRED, rollbackFor = Exception.class)
+    @Override
+    public void passFriendRequest(String sendUserId, String acceptUserId) {
+        saveFriends(sendUserId, acceptUserId);
+        saveFriends(acceptUserId, sendUserId);
+        deleteFriendRequest(sendUserId, acceptUserId);
+        deleteFriendRequest(acceptUserId, sendUserId);
+
+//        Channel sendChannel = UserChannelRel.get(sendUserId);
+//        if (sendChannel != null) {
+//            // 使用websocket主动推送消息到请求发起者，更新他的通讯录列表为最新
+//            DataContent dataContent = new DataContent();
+//            dataContent.setAction(MsgActionEnum.PULL_FRIEND.type);
+//
+//            sendChannel.writeAndFlush(
+//                    new TextWebSocketFrame(
+//                            JsonUtils.objectToJson(dataContent)));
+//        }
+    }
+
+    @Transactional(propagation = Propagation.REQUIRED, rollbackFor = Exception.class)
+    void saveFriends(String sendUserId, String acceptUserId) {
+        MyFriends myFriends = new MyFriends();
+        String recordId = sid.nextShort();
+        myFriends.setId(recordId);
+        myFriends.setMyFriendUserId(acceptUserId);
+        myFriends.setMyUserId(sendUserId);
+        myFriendsMapper.insert(myFriends);
     }
 }
