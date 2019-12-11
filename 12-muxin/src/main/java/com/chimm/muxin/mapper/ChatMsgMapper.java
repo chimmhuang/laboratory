d91345510f381728618da8cb6f077404fc5a1b3c
chimmhuang <chimmhuang@163.com>
2019-12-11 16:51:52 +0800

    feat: 新增聊天消息发送的功能

--- 12-muxin/src/main/java/com/chimm/muxin/mapper/ChatMsgMapper.java
+++ 12-muxin/src/main/java/com/chimm/muxin/mapper/ChatMsgMapper.java
@@ -1,11 +1,17 @@
 package com.chimm.muxin.mapper;
 
 import com.chimm.muxin.domain.ChatMsg;
+import org.springframework.stereotype.Repository;
 import tk.mybatis.mapper.common.Mapper;
+
+import java.util.List;
 
 /**
  * @author chimm
  * @date 2019/10/4 0004
  */
+@Repository
 public interface ChatMsgMapper extends Mapper<ChatMsg> {
+
+    public void batchUpdateMsgSigned(List<String> msgIdList);
 }
---
+++ 12-muxin/src/main/java/com/chimm/muxin/netty/UserChannelRel.java
@@ -0,0 +1,28 @@
+package com.chimm.muxin.netty;
+
+import io.netty.channel.Channel;
+
+import java.util.HashMap;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * 用户id和channel的关联关系处理
+ */
+public class UserChannelRel {
+
+    private static ConcurrentHashMap<String, Channel> manager = new ConcurrentHashMap<>();
+
+    static void put(String senderId, Channel channel) {
+        manager.put(senderId, channel);
+    }
+
+    public static Channel get(String senderId) {
+        return manager.get(senderId);
+    }
+
+    static void output() {
+        for (HashMap.Entry<String, Channel> entry : manager.entrySet()) {
+            System.out.println("UserId: " + entry.getKey() + ", ChannelId: " + entry.getValue().id().asLongText());
+        }
+    }
+}
