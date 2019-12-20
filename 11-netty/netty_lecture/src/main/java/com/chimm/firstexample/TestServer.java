5b16541287b9573d03adfc58b041e4a01fb78492
chimmhuang <chimmhuang@163.com>
2019-12-20 14:33:15 +0800

    feat: 新增netty hello world

---
+++ 11-netty/netty_lecture/src/main/java/com/chimm/firstexample/TestServer.java
@@ -0,0 +1,40 @@
+package com.chimm.firstexample;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+/**
+ * @author huangshuai
+ * @date 2019/12/19
+ */
+public class TestServer {
+
+    public static void main(String[] args) throws Exception{
+
+        // 事件循环组
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        try {
+
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+
+        /*
+            第一个 EventLoopGroup 负责接收请求
+            第二个 EventLoopGroup 负责进行实际的处理
+         */
+            serverBootstrap.group(bossGroup, workerGroup).channel(NioServerSocketChannel.class)
+                    .childHandler(new TestServerInitializer());
+
+            ChannelFuture channelFuture = serverBootstrap.bind(8899).sync();
+            channelFuture.channel().closeFuture().sync();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+
+    }
+}
---
+++ 11-netty/netty_lecture/src/main/java/com/chimm/firstexample/TestServerInitializer.java
@@ -0,0 +1,24 @@
+package com.chimm.firstexample;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.http.HttpServerCodec;
+
+/**
+ * @author huangshuai
+ * @date 2019/12/19
+ */
+public class TestServerInitializer extends ChannelInitializer<SocketChannel> {
+
+    @Override
+    protected void initChannel(SocketChannel ch) throws Exception {
+        ChannelPipeline pipeline = ch.pipeline();
+
+        /*
+            httpServerCodec：web请求编解码
+         */
+        pipeline.addLast("httpServerCodec", new HttpServerCodec());
+        pipeline.addLast("testHttpServerHandler", new TestHttpServerHandler());
+    }
+}
